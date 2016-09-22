e33ccce91fe5b9eeb98e17988d190536f505f282
martell96 <ce_martell@hotmail.com>
2016-09-22 15:22:20 -0700

    Created Alien logic.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -17,12 +18,14 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # Make a group to store the bullets in.
     bullets = Group()
+    # Make an Alien
+    alien = Alien(ai_settings, screen)
 
     # Start main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
